205e005db0573466f8c2e3c17eddea833a09cf62
Parakoopa <parakoopa@live.de>
2022-01-05 15:22:47 +0100

    Version 0.7.0

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 from setuptools import setup
 
@@ -18,12 +18,12 @@ setup(
     long_description_content_type='text/x-rst',
     url='https://github.com/Parakoopa/riptide-all/',
     install_requires=[
-        'riptide-lib >= 0.6.0, < 0.7',
-        'riptide-cli >= 0.6.0, < 0.7',
-        'riptide-proxy >= 0.6.0, < 0.7',
-        'riptide-engine-docker >= 0.6.0, < 0.7',
-        'riptide-db-mysql >= 0.6.0, < 0.7',
-        'riptide-plugin-php-xdebug >= 0.6.0, < 0.7'
+        'riptide-lib >= 0.7.0, < 0.8',
+        'riptide-cli >= 0.7.0, < 0.8',
+        'riptide-proxy >= 0.7.0, < 0.8',
+        'riptide-engine-docker >= 0.7.0, < 0.8',
+        'riptide-db-mysql >= 0.7.0, < 0.8',
+        'riptide-plugin-php-xdebug >= 0.7.0, < 0.8'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
