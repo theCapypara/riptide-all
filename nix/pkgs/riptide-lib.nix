00f4a47d31bb12a11718a550248a028478d57bf9
Marco Köpcke <marco.koepcke@tudock.de>
2024-08-06 14:43:34 +0200

    bump riptide-lib

--- nix/pkgs/riptide-lib.nix
+++ nix/pkgs/riptide-lib.nix
@@ -20,7 +20,7 @@ buildPythonPackage {
   src = fetchGit {
     url = "https://github.com/theCapypara/riptide-lib.git";
     #ref = "refs/tags/0.8.2";
-    rev = "2439e6f5698b3e0145f15bafff770536d026f231";
+    rev = "a9239f49cf88885973ad7064b21fb1f4f2296b7e";
   };
 
   propagatedBuildInputs = [
