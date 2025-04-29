cdccaf84c7552ace021f04b55081d192d572ee09
Marco Köpcke <marco.koepcke@tudock.de>
2025-04-29 10:04:09 +0200

    mysql 0.9.1

--- nix/pkgs/riptide-db-mysql.nix
+++ nix/pkgs/riptide-db-mysql.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "riptide-db-mysql";
-  version = "0.9.0";
+  version = "0.9.1";
   pyproject = true;
 
   src = fetchGit {
     url = "https://github.com/theCapypara/riptide-db-mysql.git";
     ref = "refs/tags/${version}";
-    rev = "dff15971f07a946efcb203c2f432ea490b39702a";
+    rev = "31056805dea3236ff9b8ace250d9e999e038c065";
   };
 
   nativeBuildInputs = [ setuptools ];
